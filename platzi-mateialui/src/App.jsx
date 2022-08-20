929a2162887b8fb0446d494c3d3baf542aaa69e7
mathi669 <matias.luarte@hotmail.com>
2022-08-20 13:01:56 -0400

    integracion de search

--- platzi-mateialui/src/App.jsx
+++ platzi-mateialui/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import './App.css'
+import { Container } from '@mui/material'
+import Searcher from './components/Searcher'
+import { useState } from 'react'
 
 function App() {
+
+  const [inputUser, setInputUser] = useState('octocat');
+  const [userStater, userState] = useState('inputUser');
+
   
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Container sx={{
+      background: 'whitesmoke',
+      color: 'black',
+      width: '80vw',
+      height: '600px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      margin: '200px',
+      marginBottom: '450px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}>
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+    </Container>
   )
 }
 
